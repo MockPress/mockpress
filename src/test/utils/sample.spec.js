fb8d9ea7fc17d42dfe54c8d04476e8d2a065fccf
airman5573 <rpf5573@kakao.com>
2022-12-19 13:34:06 +0900

    fix: sample test 코드 수정

--- src/test/utils/sample.spec.js
+++ src/test/utils/sample.spec.js
@@ -4,7 +4,7 @@ describe("sample", () => {
   it("should return true when length of sample is same with n", () => {
     // It's hard to say that we did a proper random test.
 
-    expect(typeof sample([1, 2, 3, 4]) === "number").toBeTruthy();
+    expect(typeof sample([1, 2, 3, 4])[0] === "number").toBeTruthy();
 
     const test1 = sample([1, 2, 3, 4], 1);
     expect(Array.isArray(test1) && test1.length === 1).toBeTruthy();
